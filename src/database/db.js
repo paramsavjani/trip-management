ca133cc42d41062d5d931ee5e82c932236295429
Param Savjani <paramsavjnai3010@gmail.com>
2024-11-05 19:49:55 +0530

    fisrt init

---
+++ src/database/db.js
@@ -0,0 +1,22 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  host: "dpg-csbsmolds78s73bf2930-a.oregon-postgres.render.com",
+  port: 5432,
+  user: "param",
+  password: "Zqy7G7GjZA04bMD7YPv1ARpKV14naBOU",
+  database: "trip_managment",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function connectDB() {
+  await client.connect();
+}
+
+async function disconnectDB() {
+  await client.end();
+}
+
+export { client, connectDB, disconnectDB };
